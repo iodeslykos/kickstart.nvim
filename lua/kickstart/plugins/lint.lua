6defc6252cc847bce66ce3ddde004217ea73dcee
iodeslykos <42@iodeslykos.com>
2025-02-03 13:31:35 -0500

    fix(lint): One tab

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -111,7 +111,7 @@ return {
           -- avoid superfluous noise, notably within the handy LSP pop-ups that
           -- describe the hovered symbol using Markdown.
           if vim.opt_local.modifiable:get() then
-              lint.try_lint()
+            lint.try_lint()
           end
         end,
       })
